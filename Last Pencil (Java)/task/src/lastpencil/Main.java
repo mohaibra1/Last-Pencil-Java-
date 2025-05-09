bf3df924f8edde6900bbaeeabfb3c65ee4b5e7c3
Moha <mimohadinho@gmail.com>
2025-05-09 11:01:57 +0200

    Stage 3/5: Working on the gameplay

--- Last Pencil (Java)/task/src/lastpencil/Main.java
+++ Last Pencil (Java)/task/src/lastpencil/Main.java
@@ -8,9 +8,23 @@ public class Main {
         System.out.println("How many pencils would you like to use:");
         int number = sc.nextInt();
         System.out.println("Who will be the first (John, Jack):");
-        String name = sc.next();
+        StringBuilder name = new StringBuilder(sc.next());
         String sticks = "|".repeat(number);
-        System.out.printf("%s%n%s is going first!", sticks, name);
+        //System.out.printf("%s%n%s is going first!", sticks, name);
+        int n = number;
+        while(number != 0){
+            System.out.printf("%s's turn:%n%s%n", name, sticks);
+            int next = sc.nextInt();
+            number = number - next;
+            sticks = "|".repeat(number);
+
+            if(name.toString().equals("Jack")){
+                name = new StringBuilder("John");
+            }else {
+                name = new StringBuilder("Jack");
+            }
+        }
+
 
     }
 }
