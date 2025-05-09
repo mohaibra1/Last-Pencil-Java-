24f2715ca3eadd3669019c8dfceff91494440e5b
Moha <mimohadinho@gmail.com>
2025-05-09 09:25:16 +0200

    Stage 1/5: Empty the pencil case

---
+++ Last Pencil (Java)/task/src/lastpencil/Main.java
@@ -0,0 +1,9 @@
+package lastpencil;
+
+public class Main {
+    public static void main(String[] args) {
+        String sticks = "||||||";
+        System.out.printf("%s%nYour turn!", sticks);
+
+    }
+}
