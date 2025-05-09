8c0c8730e936d99391e13dad739a6502f53cd574
Moha <mimohadinho@gmail.com>
2025-05-09 12:25:32 +0200

    Stage 4/5: Far play

--- Last Pencil (Java)/task/src/lastpencil/Main.java
+++ Last Pencil (Java)/task/src/lastpencil/Main.java
@@ -5,26 +5,76 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("How many pencils would you like to use:");
-        int number = sc.nextInt();
-        System.out.println("Who will be the first (John, Jack):");
-        StringBuilder name = new StringBuilder(sc.next());
-        String sticks = "|".repeat(number);
-        //System.out.printf("%s%n%s is going first!", sticks, name);
-        int n = number;
-        while(number != 0){
-            System.out.printf("%s's turn:%n%s%n", name, sticks);
-            int next = sc.nextInt();
-            number = number - next;
-            sticks = "|".repeat(number);
 
-            if(name.toString().equals("Jack")){
-                name = new StringBuilder("John");
-            }else {
-                name = new StringBuilder("Jack");
+        // Ask for number of pencils
+        int number;
+        System.out.println("How many pencils would you like to use:");
+        while (true) {
+            String input = sc.nextLine();
+
+            try {
+                number = Integer.parseInt(input);
+
+                if (number > 0) {
+                    break;
+                } else {
+                    if(number < 0){
+                        System.out.println("The number of pencils should be numeric");
+                    }else {
+                        System.out.println("The number of pencils should be positive");
+                    }
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("The number of pencils should be numeric");
             }
         }
 
+        // Ask who goes first
+        //sc.nextLine(); // Consume newline
+        String firstPlayer;
+        while (true) {
+            System.out.println("Who will be the first (John, Jack):");
+            firstPlayer = sc.nextLine();
+            if (firstPlayer.equals("John") || firstPlayer.equals("Jack")) {
+                break;
+            } else {
+                System.out.println("Choose between 'John' and 'Jack'");
+            }
+        }
 
+        String currentPlayer = firstPlayer;
+
+        while (number > 0) {
+            System.out.println("|".repeat(number));
+            System.out.printf("%s's turn!%n", currentPlayer);
+
+            int take;
+            while (true) {
+                if (sc.hasNextInt()) {
+                    take = sc.nextInt();
+                    if (take >= 1 && take <= 3) {
+                        if (take <= number) {
+                            break;
+                        } else {
+                            System.out.println("Too many pencils were taken");
+                        }
+                    } else {
+                        System.out.println("Possible values: '1', '2', or '3'");
+                    }
+                } else {
+                    System.out.println("Possible values: '1', '2', or '3'");
+                    sc.next(); // Clear invalid input
+                }
+            }
+
+            number -= take;
+
+            // Switch players
+            currentPlayer = currentPlayer.equals("John") ? "Jack" : "John";
+        }
+
+        System.out.printf("%s won!%n", currentPlayer);
+        sc.close();
     }
 }
